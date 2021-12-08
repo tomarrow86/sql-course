f188cf866d2ffb70aa12ee07e22036e510bba95c
tom <tomarrow86@gmail.com>
2021-12-09 00:21:38 +0300

    first commit

---
+++ session1.sql
@@ -0,0 +1,8 @@
+SELECT name FROM tracks;
+SELECT Composer, name FROM tracks;
+SELECT * FROM tracks;
+SELECT Composer FROM tracks;
+SELECT DISTINCT Composer FROM tracks;
+SELECT DISTINCT AlbumId, MediaTypeId FROM tracks;
+SELECT name FROM tracks WHERE Composer = 'Jimi Hendrix';
+SELECT * FROM invoices WHERE total>10;
